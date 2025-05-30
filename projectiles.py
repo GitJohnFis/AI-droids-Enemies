76711781c8ee663d66dc146832736d7a900db75b
GitJohnFis <jfisiiahi69@gmail.com>
2025-05-30 23:22:24 +1200

    Update projectiles.py

--- projectiles.py
+++ projectiles.py
@@ -1,1 +1,16 @@
+# a basic projectile firing for AI-enemies
+#class EnemyBullet:
+    #def __init__(self, x, y, target_x, target_y):
+        #self.x, self.y = x, y
+        #self.speed = 6
+        #dx, dy = target_x - self.x, target_y - self.y
+       # dist = math.sqrt(dx**2 + dy**2)
+       # self.dx, self.dy = dx/dist * self.speed, dy/dist * self.speed
 
+    #def update(self):
+       # self.x += self.dx
+       # self.y += self.dy
+
+   # def draw(self, screen):
+       # pygame.draw.circle(screen, (255, 255, 0), (int(self.x), int(self.y)), 5)
+ 
